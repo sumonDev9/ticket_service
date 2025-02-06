b742dace2c452dc2f8471cf83df6941e3cf3ed66
sumonDev9 <sumonmit678@gmail.com>
2025-02-06 10:10:52 +0530

    login page setup and password toggle

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,6 +2,7 @@
 
 // React Imports
 import { useState } from 'react'
+import 'remixicon/fonts/remixicon.css';
 
 // Next Imports
 // import Link from 'next/link'
@@ -18,17 +19,17 @@ import InputAdornment from '@mui/material/InputAdornment'
 import Checkbox from '@mui/material/Checkbox'
 import Button from '@mui/material/Button'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import Divider from '@mui/material/Divider'
+// import Divider from '@mui/material/Divider'
 
 // Component Imports
 // import Logo from '@components/layout/shared/Logo'
-import Logo from '../components/layout/shared/Logo'
+// import Logo from '../components/layout/shared/Logo'
 // import Illustrations from '@components/Illustrations'
-import Illustrations from '../components/Illustrations'
+// import Illustrations from '../components/Illustrations'
 
 // Config Imports
 // import themeConfig from '@configs/themeConfig'
-import themeConfig from '../configs/themeConfig'
+// import themeConfig from '../configs/themeConfig'
 
 // Hook Imports
 // import { useImageVariant } from '@core/hooks/useImageVariant'
@@ -55,76 +56,122 @@ const Login = ({ mode }) => {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center min-bs-[100dvh] relative p-6'>
-      <Card className='flex flex-col sm:is-[450px]'>
-        <CardContent className='p-6 sm:!p-12'>
-          <Link href='/' className='flex justify-center items-center mbe-6'>
-            <Logo />
-          </Link>
-          <div className='flex flex-col gap-5'>
-            <div>
-              <Typography variant='h4'>{`Welcome to ${themeConfig.templateName}!👋🏻`}</Typography>
-              <Typography className='mbs-1'>Please sign-in to your account and start the adventure</Typography>
-            </div>
-            <form noValidate autoComplete='off' onSubmit={handleSubmit} className='flex flex-col gap-5'>
-              <TextField autoFocus fullWidth label='Email' />
-              <TextField
-                fullWidth
-                label='Password'
-                id='outlined-adornment-password'
-                type={isPasswordShown ? 'text' : 'password'}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position='end'>
-                      <IconButton
-                        size='small'
-                        edge='end'
-                        onClick={handleClickShowPassword}
-                        onMouseDown={e => e.preventDefault()}
-                      >
-                        <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-              />
-              <div className='flex justify-between items-center gap-x-3 gap-y-1 flex-wrap'>
-                <FormControlLabel control={<Checkbox />} label='Remember me' />
-                <Typography className='text-end' color='primary' component={Link} href='/forgot-password'>
-                  Forgot password?
-                </Typography>
-              </div>
-              <Button fullWidth variant='contained' type='submit'>
-                Log In
-              </Button>
-              <div className='flex justify-center items-center flex-wrap gap-2'>
-                <Typography>New on our platform?</Typography>
-                <Typography component={Link} href='/register' color='primary'>
-                  Create an account
-                </Typography>
-              </div>
-              <Divider className='gap-3'>or</Divider>
-              <div className='flex justify-center items-center gap-2'>
-                <IconButton size='small' className='text-facebook'>
-                  <i className='ri-facebook-fill' />
-                </IconButton>
-                <IconButton size='small' className='text-twitter'>
-                  <i className='ri-twitter-fill' />
-                </IconButton>
-                <IconButton size='small' className='text-github'>
-                  <i className='ri-github-fill' />
-                </IconButton>
-                <IconButton size='small' className='text-googlePlus'>
-                  <i className='ri-google-fill' />
-                </IconButton>
-              </div>
-            </form>
+    // <div className='flex justify-center items-center min-h-screen p-6'>
+    //   <Card className='flex flex-col w-full sm:w-[450px]'>
+    //     <CardContent className='p-6 sm:!p-12'>
+       
+    //       <div className='flex flex-col gap-5'>
+    //         {/* <div>
+    //           <Typography variant='h4'>{`Welcome to ${themeConfig.templateName}!👋🏻`}</Typography>
+    //           <Typography className='mbs-1'>Please sign-in to your account and start the adventure</Typography>
+    //         </div> */}
+    //         <form noValidate autoComplete='off' onSubmit={handleSubmit} className='flex flex-col gap-5'>
+    //           <TextField autoFocus fullWidth label='Email' />
+    //           <TextField
+    //             fullWidth
+    //             label='Password'
+    //             id='outlined-adornment-password'
+    //             type={isPasswordShown ? 'text' : 'password'}
+    //             InputProps={{
+    //               endAdornment: (
+    //                 <InputAdornment position='end'>
+    //                   <IconButton
+    //                     size='small'
+    //                     edge='end'
+    //                     onClick={handleClickShowPassword}
+    //                     onMouseDown={e => e.preventDefault()}
+    //                   >
+    //                     <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
+    //                   </IconButton>
+    //                 </InputAdornment>
+    //               )
+    //             }}
+    //           />
+    //           <div className='flex justify-between items-center gap-x-3 gap-y-1 flex-wrap'>
+    //             <FormControlLabel control={<Checkbox />} label='Remember me' />
+    //           <Link>
+              
+    //           </Link>
+    //           </div>
+    //           <Button fullWidth variant='contained' type='submit'>
+    //             Log In
+    //           </Button>
+    //           <div className='flex justify-center items-center flex-wrap gap-2'>
+    //             <Typography>New on our platform?</Typography>
+    //             <Typography  className='text-blue-400'>
+    //               Create an account
+    //             </Typography>
+    //           </div>
+    //           {/* <Divider className='gap-3'>or</Divider>
+    //           <div className='flex justify-center items-center gap-2'>
+    //             <IconButton size='small' className='text-facebook'>
+    //               <i className='ri-facebook-fill' />
+    //             </IconButton>
+    //             <IconButton size='small' className='text-twitter'>
+    //               <i className='ri-twitter-fill' />
+    //             </IconButton>
+    //             <IconButton size='small' className='text-github'>
+    //               <i className='ri-github-fill' />
+    //             </IconButton>
+    //             <IconButton size='small' className='text-googlePlus'>
+    //               <i className='ri-google-fill' />
+    //             </IconButton>
+    //           </div> */}
+    //         </form>
+    //       </div>
+    //     </CardContent>
+    //   </Card>
+    //   {/* <Illustrations maskImg={{ src: authBackground }} /> */}
+    // </div>
+    <div className='flex justify-center items-center min-h-screen p-6 border-2 border-red-300'>
+  <Card className='flex flex-col w-full sm:w-[450px]'>
+    <CardContent className='p-6 sm:!p-12 w-full'>
+      <div className='flex flex-col gap-5'>
+        <form noValidate autoComplete='off' onSubmit={handleSubmit} className='flex flex-col gap-5 w-full'>
+          <TextField fullWidth label='Email' />
+          <TextField
+            fullWidth
+            label='Password'
+            id='outlined-adornment-password'
+            type={isPasswordShown ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    size='small'
+                    edge='end'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={e => e.preventDefault()}
+                  >
+                    <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
+          <div className='flex justify-between items-center gap-x-3 gap-y-1 flex-wrap'>
+            <FormControlLabel control={<Checkbox />} label='Remember me' />
+            <Link>Forgot Password?</Link>
           </div>
-        </CardContent>
-      </Card>
-      <Illustrations maskImg={{ src: authBackground }} />
-    </div>
+          <Button fullWidth variant='contained' type='submit'>
+            Log In
+          </Button>
+          <div className='flex justify-center items-center flex-wrap gap-2'>
+            <Typography>New on our platform?</Typography>
+            <Typography className='text-blue-400'>
+              Create an account
+            </Typography>
+          </div>
+        </form>
+      </div>
+    </CardContent>
+  </Card>
+</div>
   )
 }
 
 export default Login
+
+
+
+
